b447317fc6052c40f8a909b366b5e3ac29dbc65c
Carlo <carlonyc001@gmail.com>
2023-07-05 16:15:27 -0400

    tabs js to adjust size

---
+++ js/tabs.js
@@ -0,0 +1,35 @@
+// First, get all tab switches in an array
+var tabSwitches = Array.from(document.querySelectorAll('.tab-switch'));
+
+function adjustTabsHeight() {
+  // Get the .tabs div
+  var tabs = document.querySelector('.tabs');
+  // Reset the height of .tabs
+  tabs.style.height = 'auto';
+
+  // Loop through each tab switch
+  for(var i = 0; i < tabSwitches.length; i++) {
+    var tabSwitch = tabSwitches[i];
+
+    // If this tab switch is checked
+    if(tabSwitch.checked) {
+      // Get the associated .tab-content div
+      var tabContent = tabSwitch.parentNode.querySelector('.tab-content');
+  
+      // Set the height of .tabs to the height of the .tab-content
+      tabs.style.height = tabContent.offsetHeight + 'px';
+      break;
+    }
+  }
+}
+
+// Run the function initially
+adjustTabsHeight();
+
+// Run the function whenever a tab switch is clicked
+tabSwitches.forEach(function(tabSwitch) {
+  tabSwitch.addEventListener('change', adjustTabsHeight);
+});
+
+// Run the function whenever the window is resized
+window.addEventListener('resize', adjustTabsHeight);
