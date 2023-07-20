62f5097a9f7f1598221547e6494570c4e0f2657e
Carlo <carlonyc001@gmail.com>
2023-07-19 22:01:19 -0400

    tab table

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,4 +1,63 @@
-document.querySelector('.tab-dropdown').addEventListener('change', function() {
-    var tabId = this.value;
-    document.getElementById(tabId).click();
+// This function reorders the tabs and their contents when the window's width is less than or equal to 991px
+// function reorderTabs() {
+//     if (window.innerWidth <= 991) {
+//       var tabsContainer = document.querySelector('.tabs');
+
+// The for loop goes through all your tabs
+// for (var i = 1; i <= 5; i++) {
+//   var label = document.querySelector('.tab-label-' + i);
+//   var content = document.querySelector('.tab-content-' + i);
+
+// Inserts the label and its content at the end of the container
+//       tabsContainer.appendChild(label.parentElement);
+//     }
+//   }
+// }
+
+// Calls the function to reorder the tabs and contents when the page is loaded
+// window.onload = reorderTabs;
+
+// Also calls the function when the window is resized
+// window.onresize = reorderTabs;
+
+const labels = document.querySelectorAll(".accordion-item__label");
+const tabs = document.querySelectorAll(".accordion-tab");
+
+function toggleShow() {
+	const target = this;
+	const item = target.classList.contains("accordion-tab")
+		? target
+		: target.parentElement;
+	const group = item.dataset.actabGroup;
+	const id = item.dataset.actabId;
+
+	tabs.forEach(function (tab) {
+		if (tab.dataset.actabGroup === group) {
+			if (tab.dataset.actabId === id) {
+				tab.classList.add("accordion-active");
+			} else {
+				tab.classList.remove("accordion-active");
+			}
+		}
+	});
+
+	labels.forEach(function (label) {
+		const tabItem = label.parentElement;
+
+		if (tabItem.dataset.actabGroup === group) {
+			if (tabItem.dataset.actabId === id) {
+				tabItem.classList.add("accordion-active");
+			} else {
+				tabItem.classList.remove("accordion-active");
+			}
+		}
+	});
+}
+
+labels.forEach(function (label) {
+	label.addEventListener("click", toggleShow);
+});
+
+tabs.forEach(function (tab) {
+	tab.addEventListener("click", toggleShow);
 });
--- js/tabs.js
+++ js/tabs.js
@@ -1,35 +1,56 @@
 // First, get all tab switches in an array
-var tabSwitches = Array.from(document.querySelectorAll('.tab-switch'));
+// var tabSwitches = Array.from(document.querySelectorAll('.tab-switch'));
 
-function adjustTabsHeight() {
+// function adjustTabsHeight() {
   // Get the .tabs div
-  var tabs = document.querySelector('.tabs');
+  // var tabs = document.querySelector('.tabs');
   // Reset the height of .tabs
-  tabs.style.height = 'auto';
+  // tabs.style.height = 'auto';
 
   // Loop through each tab switch
-  for(var i = 0; i < tabSwitches.length; i++) {
-    var tabSwitch = tabSwitches[i];
+  // for(var i = 0; i < tabSwitches.length; i++) {
+  //   var tabSwitch = tabSwitches[i];
 
     // If this tab switch is checked
-    if(tabSwitch.checked) {
+    // if(tabSwitch.checked) {
       // Get the associated .tab-content div
-      var tabContent = tabSwitch.parentNode.querySelector('.tab-content');
+      // var tabContent = tabSwitch.parentNode.querySelector('.tab-content');
   
       // Set the height of .tabs to the height of the .tab-content
-      tabs.style.height = tabContent.offsetHeight + 'px';
-      break;
-    }
-  }
-}
+//       tabs.style.height = tabContent.offsetHeight + 'px';
+//       break;
+//     }
+//   }
+// }
 
 // Run the function initially
-adjustTabsHeight();
+// adjustTabsHeight();
 
 // Run the function whenever a tab switch is clicked
-tabSwitches.forEach(function(tabSwitch) {
-  tabSwitch.addEventListener('change', adjustTabsHeight);
-});
+// tabSwitches.forEach(function(tabSwitch) {
+//   tabSwitch.addEventListener('change', adjustTabsHeight);
+// });
 
 // Run the function whenever the window is resized
-window.addEventListener('resize', adjustTabsHeight);
+// window.addEventListener('resize', adjustTabsHeight);
+
+
+//new JS
+
+function changeTab(tabIndex) {
+  const buttons = Array.from(document.querySelectorAll('.tab-button'));
+  const contents = Array.from(document.querySelectorAll('.tab-content'));
+
+  buttons.forEach((button, index) => {
+    if (index === tabIndex) {
+      button.classList.add('active');
+      contents[index].classList.add('active');
+    } else {
+      button.classList.remove('active');
+      contents[index].classList.remove('active');
+    }
+  });
+}
+
+// Initialize the first tab as active
+changeTab(0);
